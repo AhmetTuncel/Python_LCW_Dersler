1b9a16c004d7c8c94ad5d4da6ab55205ede25298
Ahmet Tunçel <ahmet.tuncel@outlook.com>
2019-03-24 20:12:35 +0300

    Add files via upload

---
+++ Other.py
@@ -0,0 +1,39 @@
+# 1.case
+# print(2+3)
+
+# 2. case
+# num = 2+3
+
+
+# 3. case
+
+# def ortalama(list):
+#     return sum(list) / len(list)
+
+# def besEkle(list):
+#     return [i+5 for i in list]    
+
+
+
+# 4. case
+
+# def ortalama(list):
+#     return sum(list) / len(list)
+
+# def besEkle(list):
+#     return [i + 5 for i in list]
+
+# def main():
+#     print("Ortala Testi")
+#     list = [34, 44, 23, 46, 12, 24]
+#     dogruOrtalama = 30.5
+#     assert(ortalama(list) == dogruOrtalama)
+
+#     print("5 eklenmis halini test et")
+#     dogruList = [39, 49, 28, 51, 17, 29]
+#     assert(besEkle(list) == dogruList)
+
+#     print("Bütün testlerin başarı ile geçti!")
+
+# if __name__ == '__main__':
+#     main()
---
+++ demo.py
@@ -0,0 +1,39 @@
+# 1.case
+# import Other
+# print(4)
+
+# 2.case
+# import Other
+# print(Other.num)
+
+
+# 3. case
+
+# import Other
+# num = [3,11,67,99]
+
+# ort = Other.ortalama(num)
+
+# besEklenmis = Other.besEkle(num)
+
+# besEklenmisOrt = Other.ortalama(besEklenmis)
+
+# print("Ortalama:", ort)
+# print("Yeni Ortalama", besEklenmisOrt)
+
+# 4. case
+
+# import Other as o
+
+# scores = [88, 92, 79, 93, 85]
+
+# ort = o.ortalama(scores)
+# besEkle = o.besEkle(scores)
+
+# besklenmisOrtala = o.ortalama(besEkle)
+
+# print("Scores:", scores)
+# print("Ortalama:", ort, " Yeni Ortalama:", besklenmisOrtala)
+
+# print(__name__)
+# print(o.__name__)
